b178e46c93b8d709ebc37a88e7614bb1f5a98230
milkpirate <milkpirate@users.noreply.github.com>
2017-04-11 12:11:48 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -5,6 +5,7 @@ avr_types=("atmega1284p" "atmega2560")
 baudrates=(57600 115200 250000 500000 1000000 2000000)
 
 git clone https://github.com/majekw/optiboot.git
+mkdir -p ./targets
 cd optiboot/optiboot/bootloaders/optiboot/
 make clean
 
@@ -12,7 +13,7 @@ for f_cpu in "${f_cpus[@]}"; do
     for avr_type in "${avr_types[@]}"; do
         for baud in "${baudrates[@]}"; do
             F_CPU=${f_cpu}L AVR_FREQ=$f_cpu BAUD_RATE=$baud BIGBOOT=1 make $avr_type
-            mv optiboot_${avr_type}.hex ../../../../optiboot_${avr_type}_freq-${f_cpu}_baud-${baud}.hex
+            mv optiboot_${avr_type}.hex ../../../../targets/optiboot_${avr_type}_freq-${f_cpu}_baud-${baud}.hex
         done
     done
 done
